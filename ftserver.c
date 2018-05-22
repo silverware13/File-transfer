20a31e79b9d7981586622abdb073013227ca31d2
silverware13 <thomasza@oregonstate.edu>
2018-05-21 18:08:25 -0700

    improved server and client

--- ftserver.c
+++ ftserver.c
@@ -34,7 +34,7 @@
 bool check_args(int argc, char *argv[]);
 void start_up(int port_num);
 void handle_command(int connection);
-void receive_message(int connection);
+void file_transfer(int connection, char *buffer);
 
 int main(int argc, char *argv[])
 {
@@ -116,7 +116,7 @@ void start_up(int port_num)
 		//accept the next connection
 		size_client_info = sizeof(client_address); //get the size of the address for the client that will connect
 		connection = accept(listen_socket, (struct sockaddr *)&client_address, &size_client_info); //accept
-		printf("New connection\n");
+		printf("Connection from %s\n", client_address);
 		//react to clients command
 		handle_command(connection);
 	}
@@ -131,72 +131,6 @@ void start_up(int port_num)
  */
 void handle_command(int connection)
 {
-	//close the connection
-	printf("doing stuff\n");
-	receive_message(connection);
-	close(connection);
-	printf("Closed connection\n");
-}
-
-/* Function: send_message
- * --------------------------
- *  User either starts by typing a message to 
- *  the server, or user types "\quit" to end the chat.
- *
- *  socketFD: The socket number.
- *  handle: Array that holds handle.
- *  handle_size: Size of the handle array.
- *
-void send_message(int socketFD, char *handle, size_t handle_size)
-{
-	//setup variables
-	int chars_written;
-	char buffer[MAX_CHARS_MESSAGE + MAX_CHARS_HANDLE + 4];
-	char message[MAX_CHARS_MESSAGE + 1];
-	memset(buffer, '\0', MAX_CHARS_MESSAGE + MAX_CHARS_HANDLE + 4);
-	memset(message, '\0', MAX_CHARS_MESSAGE + 1);
-
-	//get message from user
-	printf("%s> ", handle); //print a prompt for the user
-	fflush(stdout); //make sure we printed the prompt
-	fgets(message, sizeof(message), stdin); //get a message from the user
-		
-	//remove newline from message, if there is none clear stdin until we find one
-	if(message[strlen(message)-1] == '\n'){
-		strtok(message, "\n"); //remove newline
-	} else {
-		char c;
-		while((c = getchar()) != '\n' && c != EOF); //clear stdin
-	}
-
-	//see if we are quiting. if we are let server know
-	if(!strcmp(message, "\\quit")){
-		send(socketFD, "\\quit\n", strlen("\\quit\n"), 0);	
-		exit(0);
-	}
-
-	snprintf(buffer, sizeof(buffer), "%s> %s\n", handle, message); //add our handle to the message
-
-	//send message to server
-	chars_written = 0;
-	do{
-		chars_written += send(socketFD, buffer, strlen(buffer), 0); //write to socket
-		if(chars_written < 0){
-			fprintf(stderr, "Error writing to socket.\n");
-			exit(2); 
-		}
-	} while(chars_written < strlen(buffer));
-}
-*/
-
-/* Function: receive_message
- * --------------------------
- *  Read a message from the client.
- *
- *  connection: The connection number.
- */
-void receive_message(int connection)
-{
 	//setup variables
 	int chars_read;
 	char buffer[MAX_CHARS_MESSAGE + 1];
@@ -206,7 +140,6 @@ void receive_message(int connection)
 	chars_read = 0;
 	int bufLen; //holds the buffer length
 	int bufSum = 0; //the number of chars we have writen to our buffer
-	printf("starting to read\n");
 	do{
 		chars_read = recv(connection, &buffer[bufSum], 100, 0); //read from socket
 		bufSum += chars_read;
@@ -223,5 +156,94 @@ void receive_message(int connection)
 	//}
 
 	//show message from server
-	printf("THIS IS THE MESSAGE: %s\n", buffer); 
+	printf("THIS IS THE MESSAGE: %s\n", buffer);
+	file_transfer(connection, buffer); 
+	close(connection);
+}
+
+/* Function: send_message
+ * --------------------------
+ *  User either starts by typing a message to 
+ *  the server, or user types "\quit" to end the chat.
+ *
+ *  socketFD: The socket number.
+ *  handle: Array that holds handle.
+ *  handle_size: Size of the handle array.
+ */
+void file_transfer(int connection, char *buffer)
+{
+	//setup variables
+	int chars_read, list_mode;
+	char portBuffer[10];
+	
+	//get the data port from the request
+	do{
+		chars_read = recv(connection, &portBuffer[bufSum], 1, 0); //read from socket
+		bufSum += chars_read;
+		bufLen = strlen(portBuffer);
+		if(chars_read < 0){
+			fprintf(stderr, "Error reading from socket.\n");
+			exit(2); 
+		}
+	} while(portBuffer[bufLen - 2] != '-');
+
+	//see if the request asks for list or file
+	if(portBuffer[bufLen - 1] == 'l')
+	{
+		list_mode = true;
+	} else {
+		list_mode = false;
+	}
+	
+	//remove the - from the portBuffer
+	portBuffer[bufLen - 2] = '\0';
+	int dataPort = strtol(portBuffer, NULL, 10);
+	printf("THIS IS THE PORT: %d\n", dataPort);
+	
+	//setup variables
+	int chars_written;
+	//setup variables
+	int socketFD, listen_socket, connection;
+	struct sockaddr_in server_address, client_address;
+	struct hostent* server_host_info;
+	socklen_t size_client_info;	
+
+	//set up the server address struct
+	memset((char*)&server_address, '\0', sizeof(server_address)); //clear address struct
+	server_address.sin_family = AF_INET; //set address family
+	server_address.sin_port = htons(dataPort); //save port number
+	server_address.sin_addr.s_addr = INADDR_ANY; //we allow connection from any address
+
+	//set up socket
+	listen_socket = socket(AF_INET, SOCK_STREAM, 0); //create the socket
+
+	//start listening with current socket for any incoming connections
+	bind(listen_socket, (struct sockaddr *)&server_address, sizeof(server_address)); //connect socket to port
+	listen(listen_socket, 10); //socket is now listening for a connection
+
+	if(list_mode){
+		printf("List directory requested on port %d\n", dataPort);	
+	} else {
+		printf("File “shortfile.txt” requested on port %d\n", dataPort);	
+	}
+
+	//accept the next connection
+	size_client_info = sizeof(client_address); //get the size of the address for the client that will connect
+	connection = accept(listen_socket, (struct sockaddr *)&client_address, &size_client_info); //accept
+	printf("Connection from %s\n", client_address);
+	//react to clients command
+	handle_command(connection);
+	
+	//setup variables
+	int chars_written;
+
+	//send file to client
+	chars_written = 0;
+	do{
+		chars_written += send(connection, buffer, strlen(buffer), 0); //write to socket
+		if(chars_written < 0){
+			fprintf(stderr, "Error writing to socket.\n");
+			exit(2); 
+		}
+	} while(chars_written < strlen(buffer));
 }
